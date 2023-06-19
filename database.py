12bb9f8f19a9b981e37d91e6dc4e618fb8ac55da
Eran <eranshalev10@gmail.com>
2023-06-20 00:35:27 +0300

    finished all tables creation

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
 import sqlite3
+import math
+import datetime
 
 """
-Tables (static means it's values shouldn't be changed):
-1) names (static): cols = id (unique), name,
-2) hierarchy (static): cols = id (unique), parent, children,
+########################################################################################################################
+Constants:
+########################################################################################################################
 """
+TEST_1_WEIGHT = 0.1
+TEST_2_WEIGHT = 0.5
+TEST_3_WEIGHT = 0.4
 
+"""
+########################################################################################################################
+Database Operations:
+########################################################################################################################
+"""
 # setup
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
@@ -14,10 +24,9 @@ c = conn.cursor()
 # ----------------------------------------------------------------------------------------------------------------------
 # create table
 
-# c.execute("""CREATE TABLE hierarchy(
+# c.execute("""CREATE TABLE text(
 #              id TEXT PRIMARY KEY UNIQUE,
-#              parent TEXT,
-#              children TEXT
+#              text TEXT
 # )""")
 # conn.commit()
 # ----------------------------------------------------------------------------------------------------------------------
@@ -26,11 +35,12 @@ c = conn.cursor()
 # insert
 
 # values = [
-#     ("berakhot_1_1", "berakhot_1", None)
+#     ("berakhot_1_2", mishnayotText.berakhot_1_2_text),
+#     ("berakhot_1_3", mishnayotText.berakhot_1_3_text)
 # ]
 #
 # # check table name is correct here
-# c.executemany('INSERT INTO <hierarchy> (id, parent, children) VALUES (?, ?, ?)', values)
+# c.executemany('INSERT INTO text (id, text) VALUES (?, ?)', values)
 # conn.commit()
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -52,15 +62,9 @@ c = conn.cursor()
 # ----------------------------------------------------------------------------------------------------------------------
 # select
 # c.execute("""SELECT * FROM hierarchy WHERE id='berakhot_8'""")
-# c.execute("""SELECT * FROM hierarchy""")
+# c.execute("""SELECT * FROM grades""")
 # for line in c.fetchall():
 #     print(line)
-# ----------------------------------------------------------------------------------------------------------------------
-
-# ----------------------------------------------------------------------------------------------------------------------
-# drop table
-# c.execute('DROP TABLE <table name>')
-# conn.commit()
 # ----------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -80,6 +84,147 @@ c = conn.cursor()
 conn.close()
 
 """
+########################################################################################################################
+Database Functions:
+########################################################################################################################
+"""
+
+
+def show_all_table_rows(table):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table}")
+    for line in cursor.fetchall():
+        print(line)
+    connection.close()
+
+
+def get_all_table_rows(table):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table}")
+    lines = []
+    for line in cursor.fetchall():
+        lines.append(line)
+    connection.close()
+    return lines
+
+
+def update_test_1(mishna_id, grade):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    new_value = grade
+    row_id = mishna_id
+    cursor.execute("UPDATE grades SET test_1_grade = ? WHERE id = ?", (new_value, row_id))
+    connection.commit()
+    cursor.execute("SELECT * FROM grades WHERE id = ?", (mishna_id,))
+    row = list(cursor.fetchone())
+    new_value = math.floor(TEST_1_WEIGHT * row[1] + TEST_2_WEIGHT * row[2] + TEST_3_WEIGHT * row[3])
+    cursor.execute("UPDATE grades SET final_grade = ? WHERE id = ?", (new_value, row_id))
+    connection.commit()
+    connection.close()
+
+
+def reset_test_1(mishna_id):
+    update_test_1(mishna_id, 0)
+
+
+def reset_all_test_1():
+    rows = get_all_table_rows("grades")
+    for row in rows:
+        mishna_id = row[0]
+        reset_test_1(mishna_id)
+
+
+def update_test_2(mishna_id, grade):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    new_value = grade
+    row_id = mishna_id
+    cursor.execute("UPDATE grades SET test_2_grade = ? WHERE id = ?", (new_value, row_id))
+    connection.commit()
+    cursor.execute("SELECT * FROM grades WHERE id = ?", (mishna_id,))
+    row = list(cursor.fetchone())
+    new_value = math.floor(TEST_1_WEIGHT * row[1] + TEST_2_WEIGHT * row[2] + TEST_3_WEIGHT * row[3])
+    cursor.execute("UPDATE grades SET final_grade = ? WHERE id = ?", (new_value, row_id))
+    connection.commit()
+    connection.close()
+
+
+def reset_test_2(mishna_id):
+    update_test_2(mishna_id, 0)
+
+
+def reset_all_test_2():
+    rows = get_all_table_rows("grades")
+    for row in rows:
+        mishna_id = row[0]
+        reset_test_2(mishna_id)
+
+
+def update_last_100_score_date(mishna_id, date_in_timestamp):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    new_value = date_in_timestamp
+    row_id = mishna_id
+    cursor.execute("UPDATE dates SET last_100_score = ? WHERE id = ?", (new_value, row_id))
+    connection.commit()
+    connection.close()
+
+
+def update_last_100_score_date_now(mishna_id):
+    dt = datetime.datetime.now()
+    seconds = int(dt.timestamp())
+    update_last_100_score_date(mishna_id, seconds)
+
+
+def reset_last_100_score_date(mishna_id):
+    update_last_100_score_date(mishna_id, None)
+
+
+def reset_all_last_100_score_date():
+    rows = get_all_table_rows("dates")
+    for row in rows:
+        mishna_id = row[0]
+        reset_last_100_score_date(mishna_id)
+
+
+def drop_table(table):
+    if table in ["names", "hierarchy", "grades", "dates", "text"]:
+        raise Exception("are you sure??")
+    else:
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute(f"DROP TABLE {table}")
+        connection.commit()
+        connection.close()
+
+
+def get_text(mishna_id):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM text WHERE id='{mishna_id}'")
+    text = cursor.fetchone()[1]
+    cursor.close()
+    return text
+
+show_all_table_rows("text")
+"""
+########################################################################################################################
+Notes:
+########################################################################################################################
+"""
+
+"""
+Tables (static means it's values shouldn't be changed):
+1) names (static): cols = id (unique), name
+2) hierarchy (static): cols = id (unique), parent, children
+3) grades (dynamic): cols = id (unique), test_1_grade, test_2_grade, test_3_grade, final_grade
+4) dates (dynamic): cols = id (unique), last_100_score
+5) text (static): cols = id (unique), text
+"""
+
+"""
 id values to remember:
 shas
 orders: zeraim, moed, nashim, nezikin, kodashim, tohorot
@@ -94,4 +239,64 @@ for line in c.fetchall():
     list_children = [e.strip() for e in list_children]
     for child in list_children:
         print(child)
+"""
+
+"""
+#     ("berakhot_1_1", None),
+#     ("berakhot_1_2", None),
+#     ("berakhot_1_3", None),
+#     ("berakhot_1_4", None),
+#     ("berakhot_1_5", None),
+#     ("berakhot_2_1", None),
+#     ("berakhot_2_2", None),
+#     ("berakhot_2_3", None),
+#     ("berakhot_2_4", None),
+#     ("berakhot_2_5", None),
+#     ("berakhot_2_6", None),
+#     ("berakhot_2_7", None),
+#     ("berakhot_2_8", None),
+#     ("berakhot_3_1", None),
+#     ("berakhot_3_2", None),
+#     ("berakhot_3_3", None),
+#     ("berakhot_3_4", None),
+#     ("berakhot_3_5", None),
+#     ("berakhot_3_6", None),
+#     ("berakhot_4_1", None),
+#     ("berakhot_4_2", None),
+#     ("berakhot_4_3", None),
+#     ("berakhot_4_4", None),
+#     ("berakhot_4_5", None),
+#     ("berakhot_4_6", None),
+#     ("berakhot_4_7", None),
+#     ("berakhot_5_1", None),
+#     ("berakhot_5_2", None),
+#     ("berakhot_5_3", None),
+#     ("berakhot_5_4", None),
+#     ("berakhot_5_5", None),
+#     ("berakhot_6_1", None),
+#     ("berakhot_6_2", None),
+#     ("berakhot_6_3", None),
+#     ("berakhot_6_4", None),
+#     ("berakhot_6_5", None),
+#     ("berakhot_6_6", None),
+#     ("berakhot_6_7", None),
+#     ("berakhot_6_8", None),
+#     ("berakhot_7_1", None),
+#     ("berakhot_7_2", None),
+#     ("berakhot_7_3", None),
+#     ("berakhot_7_4", None),
+#     ("berakhot_7_5", None),
+#     ("berakhot_8_1", None),
+#     ("berakhot_8_2", None),
+#     ("berakhot_8_3", None),
+#     ("berakhot_8_4", None),
+#     ("berakhot_8_5", None),
+#     ("berakhot_8_6", None),
+#     ("berakhot_8_7", None),
+#     ("berakhot_8_8", None),
+#     ("berakhot_9_1", None),
+#     ("berakhot_9_2", None),
+#     ("berakhot_9_3", None),
+#     ("berakhot_9_4", None),
+#     ("berakhot_9_5", None)
 """
