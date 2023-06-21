30af6b984cb9ff6dfd7bb3f35588e93f21992c02
Eran <eranshalev10@gmail.com>
2023-06-21 23:01:15 +0300

    branch start in testing

--- extraModules.py
+++ extraModules.py
@@ -3,9 +3,7 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-import mishnayotText
 import extraFunctions
-from statistics import mean
 from kivy.utils import get_color_from_hex
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
